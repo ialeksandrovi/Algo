561a11fd40e001a046156bc7f12420c75058ff31
ialeksandrovi <ialexandrov@dejavoosystems.ru>
2021-03-20 00:25:56 +0700

    finished hm

---
+++ src/les2/ClassForTask.java
@@ -0,0 +1,15 @@
+package les2;
+
+public class ClassForTask {
+    private int argument;
+    static int a = 33;
+    static Integer ref = 121;
+
+    public ClassForTask(int argument) {
+        this.argument = argument;
+    }
+
+    public int getArgument() {
+        return argument;
+    }
+}

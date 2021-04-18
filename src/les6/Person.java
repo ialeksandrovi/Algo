282b2c9e42df0b7f594078472ba83c8097b89f8c
ialeksandrovi <ialexandrov@dejavoosystems.ru>
2021-04-18 23:37:44 +0700

    lesson6

---
+++ src/les6/Person.java
@@ -0,0 +1,13 @@
+package les6;
+
+class Person {
+    public int id;
+    public String name;
+    public int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
